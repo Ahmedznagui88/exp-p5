61d9c800fc9249f4869fc81637bfa213fb3c5d3a
Ahmed Znagui <amed.bob88@gmail.com>
2024-02-29 20:46:30 +0100

    start p5

---
+++ sketch.js
@@ -0,0 +1,36 @@
+function setup() {
+  createCanvas(500, 500);
+  
+}
+
+
+function draw() {
+  background(235);
+  
+  stroke(125, 120, 0);
+  line(100, 100, 100, 400);
+  line(200, 150, 200, 350); 
+  line(300, 200, 300, 300);
+  line(100, 100,400, 250 )
+  line(100, 400, 400, 250);
+  
+  /* fill(225, 120, 0);
+  let x = frameCount % 100;
+  let y = frameCount % 100;
+
+  x = map(x, 0, 100, 0, width);
+  y = map(y, 0, 140, 0, height);
+
+
+  if (mouseIsPressed === true) {
+    frameRate(10);
+  } else {
+    frameRate(800);
+  };
+  ellipse(x, y, 100, 100); */
+  
+
+  
+  
+}
+

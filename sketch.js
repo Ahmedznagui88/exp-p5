251615bb06305fcdcd2ad0b2f6f2e9329a17f1c9
Ahmed Znagui <amed.bob88@gmail.com>
2024-02-29 22:11:04 +0100

    bruno munari face

--- sketch.js
+++ sketch.js
@@ -1,3 +1,8 @@
+
+let yMouth = 375; //mouth position
+let dEyes = 30; //eyes position
+let yEyes = 150; // eyes size
+
 function setup() {
   createCanvas(500, 500);
   angleMode(DEGREES); //!angleMode prende solo un parametro
@@ -6,7 +11,7 @@ function setup() {
 
 function draw() {
   background(220);
-  noStroke();
+
   //rect rosso
   fill('#EE4266')
   rect(50, 50, 200, 400);
@@ -17,14 +22,44 @@ function draw() {
   fill('#7ED7C1')
   rect(250, 250, 100, 100);
   rect(350, 350, 100, 100);
-  
-  
 
+  strokeWeight(4);
+  stroke(0);
+  //vertical line
+  line(50, 50, 50, 450);
+  line(150, 50, 150, 450);
+  line(250, 50, 250, 450);
+  line(350, 50, 350, 450);
+  line(450, 50, 450, 450);
+  //horizontal line
+  line(50, 50, 450, 50);
+  line(50, 150, 450, 150);
+  line(50, 250, 450, 250);
+  line(50, 350, 450, 350);
+  line(50, 450, 450, 450);
 
- 
-  
+  //left eyebrow
+  line(50, 150, 150, 50);  
+  line(150, 50, 250, 150);
+  //right eyebrow
+  line(250, 150, 350, 50);
+  line(350, 50, 450, 150);
 
-  
-  
+  //nose
+  line(150, 250, 250, 350);
+  line(250, 350, 350, 250);
+
+  //mouth
+  line(150, yMouth, 350, yMouth);
+
+  //eyes
+  fill(0)
+  noStroke();
+  ellipse(150, yEyes, dEyes, dEyes);
+  ellipse(350, yEyes, dEyes, dEyes);
 }
 
+function keyPressed(){
+  console.log("keyPressed")
+  save('face_bruno_marini.png')
+}
